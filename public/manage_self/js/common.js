90127e84ba907f94dafbebf9c8596fe1fd0b4bd4
hulinghao <hulinghao0798@gmail.com>
2017-12-09 00:34:30 +0800

    实现首页公共部分 抽取为公共js 公共css 登陆 登出

--- public/manage_self/js/common.js
+++ public/manage_self/js/common.js
@@ -2,13 +2,67 @@
 
 // 为ajax注册全局事件
 // 开始ajax时
-$(document).ajaxStart(function(){
+$(document).ajaxStart(function () {
     NProgress.start();
 })
 
 
 // ajax结束时
-$(document).ajaxStop(function(){
+$(document).ajaxStop(function () {
     NProgress.done();
 })
 
+if (window.location.href.indexOf("login.html") == -1) {
+    // 验证是否登陆
+    $.ajax({
+        url: '/employee/checkRootLogin',
+        success: function (backData) {
+            console.log(backData);
+            // 如果没有登陆
+            if ( backData.error == 400) {
+                // 打回登陆页
+                window.location.href = './login.html';
+            }
+        }
+    })
+}
+
+
+
+// 显示 隐藏侧边栏
+$('.lt_top a:first').click(function () {
+    $('.lt_aside').toggleClass('fullScreen');
+    $('.lt_main').toggleClass('fullScreen');
+})
+
+// 侧边栏功能实现
+/*
+    不需要点击切换高亮
+    只需要 实现 展开 收起功能即可
+*/
+$(".menu ol").parent().click(function () {
+    console.log('分类按钮');
+    $(this).children('ol').slideToggle();
+})
+
+// 弹出modal
+$(".glyphicon-log-out").click(function (event) {
+    event.preventDefault();
+    // 弹出modal框
+    // 判断页面是否有modal
+    $(".modal").modal('show');
+})
+
+// 点击modal的确定按钮
+$('.modal button.btn-danger').click(function () {
+    // 调用登出接口
+    $.ajax({
+        url: '/employee/employeeLogout',
+        success: function (backData) {
+            console.log(backData);
+            window.location.href = './login.html';
+        }
+    })
+
+    // 去登陆页
+})
